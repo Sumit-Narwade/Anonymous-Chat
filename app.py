4e025dcf1abac6f3f5bb01fa26aa4f4361260d89
Sumit-Narwade <narwadesumit@gmail.com>
2025-11-11 22:28:36 +0530

    added a main host routing

--- app.py
+++ app.py
@@ -1,46 +1,82 @@
 from flask import Flask, render_template, request
+
 from flask_socketio import SocketIO, send, emit
 
+
 app = Flask(__name__)
+
 app.secret_key = 'secret!'
+
 socketio = SocketIO(app)
 
+
 # username -> set of session IDs
+
 user_sessions = {}
 
+
 @app.route('/')
+
 def index():
+
     return render_template('index.html')
 
+
 @socketio.on('join')
+
 def handle_join(username):
+
     sid = request.sid
+
     if username not in user_sessions:
+
         user_sessions[username] = set()
+
     user_sessions[username].add(sid)
+
     emit('status', f"{username} joined the chat", broadcast=True)
 
+
 @socketio.on('message')
+
 def handle_message(data):
+
     username = data.get('username', 'Anonymous')
+
     message = data.get('message', '')
+
     send({'username': username, 'message': message}, broadcast=True)
 
+
 @socketio.on('disconnect')
+
 def handle_disconnect():
+
     sid = request.sid
+
     username_to_remove = None
 
+
     for username, sessions in list(user_sessions.items()):
+
         if sid in sessions:
+
             sessions.remove(sid)
+
             if not sessions:  # no sessions left for this user
+
                 username_to_remove = username
+
                 del user_sessions[username]
+
             break
 
+
     if username_to_remove:
+
         emit('status', f"{username_to_remove} left the chat", broadcast=True)
 
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+
+    socketio.run(app, host='0.0.0.0', port=5000)
